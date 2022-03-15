bd3e575dbb5f83442348732689352c3c7fce566d
Joshua Ascoli <100535893+JoshuaAscoli@users.noreply.github.com>
2022-03-15 09:59:18 -0600

    Revert "Develop"

--- home.py
+++
@@ -1,7 +0,0 @@
-#Ingreso mis datos
-print('Mi nombre es Joshua Ascoli')
-print('Mi numero de carne es 1515522')
-#Datos inge o colaborador
-Print('')
-Print('')
-print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
